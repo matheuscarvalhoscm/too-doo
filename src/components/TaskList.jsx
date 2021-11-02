1d25c72170f945db1f9427566597dbeed7ca965d
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-11-02 18:16:24 -0300

    correct poor indentation

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,26 +1,23 @@
-import React, { useContext, useEffect } from 'react';
-import TooDooContext from '../context/TooDooContext';
-import TaskListButtons from './TaskListButtons';
+import React, { useContext, useEffect } from "react";
+import TooDooContext from "../context/TooDooContext";
+import TaskListButtons from "./TaskListButtons";
 
 function TaskList() {
   const { taskList } = useContext(TooDooContext);
 
   if (taskList !== [] || taskList !== undefined)
-	return(
-		<div>
-			<ul>
-        {taskList.map((task) => 
-          <div>
-            <li>
-              { task }
-            </li>
-            <TaskListButtons />
-          </div>
-        )
-        }
-			</ul>
-		</div>
-	);
+    return (
+      <div>
+        <ul>
+          {taskList.map((task) => (
+            <div>
+              <li>{task}</li>
+              <TaskListButtons />
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
 }
 
 export default TaskList;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Input from '../components/Input';
-import TaskList from '../components/TaskList';
-import Footer from '../components/Footer';
+import React from "react";
+import Input from "../components/Input";
+import TaskList from "../components/TaskList";
+import Footer from "../components/Footer";
 
 function MainPage() {
-	return(
-		<div>
-		  <h1>Too Doo</h1>
-		  <Input />
-		  <TaskList />
-		  <Footer />
-		</div>
-	);
+  return (
+    <div>
+      <h1>Too Doo</h1>
+      <Input />
+      <TaskList />
+      <Footer />
+    </div>
+  );
 }
 
 export default MainPage;
