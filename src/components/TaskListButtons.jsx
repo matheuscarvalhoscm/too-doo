7034dff712c6bf72bfc0e5a5a2268f2b38fc7cc6
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-12-16 20:53:18 -0300

    add delete item logic

--- src/components/TaskListButtons.jsx
+++ src/components/TaskListButtons.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{ useContext } from 'react';
+import TooDooContext from '../context/TooDooContext'
 
-function TaskListButtons() {
+function TaskListButtons({ id }) {
+  const {taskList, setTaskList } = useContext(TooDooContext);
+
+  const handleDelete = (id) => {
+    const newTaskList = taskList.filter((task) => task.id !== id);
+    setTaskList(newTaskList);
+  }
+  
   return(
     <div className="task-buttons">
-      <button className="concluded-button" type="button" />
+      <input className="concluded-button" type="checkbox" />
       <button className="edit-button" type="button" />
-      <button className="delete-button" type="button" />
+      <button className="delete-button" type="button" onClick={ () => handleDelete(id) } />
     </div>
   );
 }
